e22bcbbb6042c0f4ed7e4a63588b3607273cda4b
ShaadSD <shaad5843@gmail.com>
2025-08-07 13:38:20 +0600

    concludary url change

--- product/serializers.py
+++ product/serializers.py
@@ -3,7 +3,12 @@ from .models import Product,Category
 
 class ProductSerializers(serializers.ModelSerializer):
     category = serializers.PrimaryKeyRelatedField(queryset=Category.objects.all(), many=True)
+    image = serializers.SerializerMethodField()
 
+    def get_image(self,obj):
+        if obj.image:
+            return obj.image.url
+        return None
     class Meta:
         model = Product
         fields = ['id', 'name', 'category', 'slug', 'description', 'price', 'Old_price', 'discount', 'unit', 'image', 'available']
