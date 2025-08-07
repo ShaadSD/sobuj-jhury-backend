5c41befdb521110b502afd1fe8a50c0d8b8e44a1
ShaadSD <shaad5843@gmail.com>
2025-08-07 12:13:22 +0600

    clodunary cng

--- user/models.py
+++ user/models.py
@@ -1,9 +1,9 @@
 from django.db import models
 from django.contrib.auth.models import User
-
+from cloudinary.models import CloudinaryField
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    image = models.URLField(max_length=200,blank=True, null=True)
+    image = CloudinaryField('image', blank=True, null=True)
     mobile_no = models.CharField(max_length=15,blank=True, null=True)
     location = models.CharField(max_length=100, blank=True, null=True)
 
